16597d417dc762eb15ffa0e1f0699c72e79501b2
ex_panleiming <1025624185@qq.com>
2017-01-22 16:31:10 +0800

    将排序的创建移出，通过简单工厂模式进行实现

---
+++ src/com/plm/factory/SortFactory.java
@@ -0,0 +1,45 @@
+package com.plm.factory;
+
+import com.plm.sort.base.Sort;
+import com.plm.sort.imp.BubbleSort;
+import com.plm.sort.imp.SelectionSort;
+import com.plm.sort.util.SortVariety;
+
+/**
+ * 排序工厂，使用单例模式进行急切构建
+ * @author ex_panleiming
+ *
+ */
+public class SortFactory {
+	private static SortFactory factory = new SortFactory();
+	
+	/**
+	 * 构造方法进行私有化
+	 */
+	private SortFactory(){
+		
+	}
+	
+	/**
+	 * 工厂初始化并返回实例
+	 * @return
+	 */
+	public static SortFactory newInstance(){
+		return factory;
+	}
+	
+	/**
+	 * 根据选择的排序中类，返回排序类
+	 * @param variety
+	 * @return
+	 */
+	public Sort createSort(SortVariety variety){
+		if(variety == SortVariety.bubble){
+			return new BubbleSort();
+		}else if(variety == SortVariety.selection){
+			return new SelectionSort();
+		}else{
+			return null;
+		}
+	}
+}
---
+++ src/com/plm/sort/util/SortVariety.java
@@ -0,0 +1,11 @@
+package com.plm.sort.util;
+
+/**
+ * 排序种类
+ * @author ex_panleiming
+ *
+ */
+public enum SortVariety {
+	bubble,//冒泡 
+	selection;//选择
+}
